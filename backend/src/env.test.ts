740472d7aa03bcd7632c10822defa3263aaaab14
Matt Mannion <mattpmannion@gmail.com>
2022-07-12 07:16:56 -0400

    mm - updated macks tests

--- backend/src/env.test.ts
+++ backend/src/env.test.ts
@@ -1,14 +1,6 @@
-import { cfg } from 'src/env';
+process.env.NODE_ENV = 'prod';
+import { prod } from 'src/env';
 
-describe('checks if cors whitelist works in dev env, and redis url returning expected string', function () {
-  it('should return proper redis url', function () {
-    const { host, port, url } = cfg.redis;
-
-    expect(url).toBe(`redis://${host}:${port}`);
-  });
-
-  it('test whitelist for dev', function () {
-    const { devlist, whitelist } = cfg.cors;
-    expect(whitelist).toBe(devlist);
-  });
+it('tests for prod', function () {
+  expect(prod).toBe(true);
 });
